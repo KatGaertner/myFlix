38ad838fb4c969821b316812ed39fa6b81cc0d59
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-06-21 11:02:27 +0200

    implemented basic redux

--- src/redux/reducers/movies.js
+++ src/redux/reducers/movies.js
@@ -4,10 +4,7 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: [],
   reducers: {
-    setMovies: (state, action) => {
-      console.log(state, state.movies, action.payload)
-      return state.movies.push(action.payload)
-    }
+    setMovies: (state, action) => action.payload
   }
 });
 
