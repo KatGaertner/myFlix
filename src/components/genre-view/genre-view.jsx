d9862b19bf6bd071542ac41173f745b9bf497df5
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-07-21 19:53:37 +0200

    added loading screen

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -6,6 +6,7 @@ import { API } from "../../utils/links";
 import { MovieGrid } from "../movie-grid/movie-grid";
 import { useSelector } from "react-redux";
 import { checkAuth } from "../../utils/fetchErrorHandlers";
+import { LoadingInfo } from "../main-view/loading-info";
 
 export const GenreView = () => {
   const movies = useSelector((state) => state.movies.list);
@@ -15,6 +16,7 @@ export const GenreView = () => {
   const genreMovies = movies.filter(
     (movie) => movie.genres[0].name === genreName
   );
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${API}/genres/${genreName}`, {
@@ -26,26 +28,36 @@ export const GenreView = () => {
       })
       .then((data) => {
         setgenreData(data[0]);
+        setIsLoading(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <>
-      <Row>
-        <Col className="d-flex flex-column justify-content-between">
-          <h1>{genreData.name}</h1>
-          <div className="mb-2">{genreData.description}</div>
+      {isLoading ? (
+        <LoadingInfo />
+      ) : (
+        <>
+          <Row>
+            <Col className="d-flex flex-column justify-content-between">
+              <h1>{genreData.name}</h1>
+              <div className="mb-2">{genreData.description}</div>
+              <div>
+                <BackButton />
+              </div>
+            </Col>
+          </Row>
+          <hr />
           <div>
-            <BackButton />
+            <p>Movies with the genre {genreData.name}:</p>
+            <MovieGrid movies={genreMovies} />
           </div>
-        </Col>
-      </Row>
-      <hr />
-      <div>
-        <p>Movies with the genre {genreData.name}:</p>
-        <MovieGrid movies={genreMovies} />
-      </div>
+        </>
+      )}
     </>
   );
 };
