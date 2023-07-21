f77d203e2b84fe33a57e133d494e3c270ec3226c
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-07-21 19:53:37 +0200

    added loading screen

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -6,6 +6,7 @@ import { API } from "../../utils/links";
 import { MovieGrid } from "../movie-grid/movie-grid";
 import { useSelector } from "react-redux";
 import { checkAuth } from "../../utils/fetchErrorHandlers";
+import { LoadingInfo } from "../main-view/loading-info";
 
 export const DirectorView = () => {
   const movies = useSelector((state) => state.movies.list);
@@ -15,6 +16,7 @@ export const DirectorView = () => {
   const directorMovies = movies.filter(
     (movie) => movie.directors[0].name === directorName
   );
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${API}/directors/${directorName}`, {
@@ -26,33 +28,43 @@ export const DirectorView = () => {
       })
       .then((data) => {
         setDirectorData(data[0]);
+        setIsLoading(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <>
-      <Row>
-        <Col className="d-flex flex-column justify-content-between">
-          <h1>{directorData.name}</h1>
-          <div className="fst-italic mb-2">
-            {[directorData.birthyear, directorData.deathyear].reduce(
-              (birth, death) => {
-                return "*" + birth + (death ? ",  \u271D" + death : "");
-              }
-            )}
-          </div>
-          <div className="mb-2">{directorData.biography}</div>
+      {isLoading ? (
+        <LoadingInfo />
+      ) : (
+        <>
+          <Row>
+            <Col className="d-flex flex-column justify-content-between">
+              <h1>{directorData.name}</h1>
+              <div className="fst-italic mb-2">
+                {[directorData.birthyear, directorData.deathyear].reduce(
+                  (birth, death) => {
+                    return "*" + birth + (death ? ",  \u271D" + death : "");
+                  }
+                )}
+              </div>
+              <div className="mb-2">{directorData.biography}</div>
+              <div>
+                <BackButton />
+              </div>
+            </Col>
+          </Row>
+          <hr />
           <div>
-            <BackButton />
+            <p>Movies by {directorData.name}:</p>
+            <MovieGrid movies={directorMovies} />
           </div>
-        </Col>
-      </Row>
-      <hr />
-      <div>
-        <p>Movies by {directorData.name}:</p>
-        <MovieGrid movies={directorMovies} />
-      </div>
+        </>
+      )}
     </>
   );
 };
