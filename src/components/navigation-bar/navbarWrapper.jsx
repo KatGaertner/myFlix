bcdac5e860b12e636189b5caa1ee716b95bfd060
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-07-21 03:13:20 +0200

    added conditions for collapsing navbar

---
+++ src/components/navigation-bar/navbarWrapper.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const useOutsideAlerter = (ref, handleToggle) => {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handleToggle();
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [ref]);
+};
+
+export const OutsideAlerter = (props) => {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, props.handleToggle);
+
+  return <div ref={wrapperRef}>{props.children}</div>;
+};
+
+OutsideAlerter.propTypes = {
+  props: PropTypes.object.isRequired,
+};
--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,74 +3,89 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/reducers/userData";
 import PropTypes from "prop-types";
+import { OutsideAlerter } from "./navbarWrapper";
 
 export const NavigationBar = ({ isLoading }) => {
   const token = useSelector((state) => state.userData.token);
   const dispatch = useDispatch();
 
-  return (
-    <Navbar
-      bg="primary"
-      expand="md"
-      className="navbar-light w-100 bg-gradient my-navbar fixed-top"
-      variant="light"
-    >
-      <Container fluid className="py-1 px-3">
-        <Navbar.Brand as={Link} to="/" className="icon-link">
-          <img
-            src={require("./logo.svg")}
-            alt="Logo"
-            width={"32px"}
-            height={"32px"}
-          />
+  const handleToggle = () => {
+    if (
+      !document.getElementById("navbar-toggler").classList.contains("collapsed")
+    ) {
+      document.getElementById("navbar-toggler").click();
+    }
+  };
 
-          <span style={{ fontFamily: "'Comfortaa', cursive" }}>myFlix</span>
-        </Navbar.Brand>
-        {isLoading ? (
-          <></>
-        ) : (
-          <>
-            <Navbar.Toggle aria-controls="navbar" />
-            <Navbar.Collapse id="navbar">
-              {!token ? (
-                <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/login">
-                    Log in
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/signup">
-                    Sign up
-                  </Nav.Link>
-                </Nav>
-              ) : (
-                <>
-                  <Nav className="align-items-top">
-                    <Nav.Link as={Link} to="/">
-                      Home
+  return (
+    <OutsideAlerter handleToggle={handleToggle}>
+      <Navbar
+        bg="primary"
+        expand="md"
+        className="navbar-light w-100 bg-gradient my-navbar fixed-top"
+        variant="light"
+      >
+        <Container fluid className="px-3">
+          <Navbar.Brand as={Link} to="/" className="icon-link">
+            <img
+              src={require("./logo.svg")}
+              alt="Logo"
+              width={"32px"}
+              height={"32px"}
+            />
+
+            <span style={{ fontFamily: "'Comfortaa', cursive" }}>myFlix</span>
+          </Navbar.Brand>
+          {isLoading ? (
+            <></>
+          ) : (
+            <>
+              <Navbar.Toggle
+                aria-controls="navbarCollapse"
+                id="navbar-toggler"
+              />
+              <Navbar.Collapse id="navbarCollapse">
+                {!token ? (
+                  <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/login" onClick={handleToggle}>
+                      Log in
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/profile">
-                      Profile
+                    <Nav.Link as={Link} to="/signup" onClick={handleToggle}>
+                      Sign up
                     </Nav.Link>
                   </Nav>
-                  <Form className="d-flex flex-column flex-md-row flex-fill align-items-top">
-                    <Button
-                      variant="outline-dark"
-                      className="btn ms-auto"
-                      onClick={() => {
-                        dispatch(logoutUser());
-                      }}
-                    >
-                      <span style={{ fontFamily: "'Comfortaa', cursive" }}>
-                        Logout
-                      </span>
-                    </Button>
-                  </Form>
-                </>
-              )}
-            </Navbar.Collapse>
-          </>
-        )}
-      </Container>
-    </Navbar>
+                ) : (
+                  <>
+                    <Nav className="align-items-top">
+                      <Nav.Link as={Link} to="/" onClick={handleToggle}>
+                        Home
+                      </Nav.Link>
+                      <Nav.Link as={Link} to="/profile" onClick={handleToggle}>
+                        Profile
+                      </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex flex-column flex-md-row flex-fill align-items-top">
+                      <Button
+                        variant="outline-dark"
+                        className="btn ms-auto"
+                        onClick={() => {
+                          handleToggle();
+                          dispatch(logoutUser());
+                        }}
+                      >
+                        <span style={{ fontFamily: "'Comfortaa', cursive" }}>
+                          Logout
+                        </span>
+                      </Button>
+                    </Form>
+                  </>
+                )}
+              </Navbar.Collapse>
+            </>
+          )}
+        </Container>
+      </Navbar>
+    </OutsideAlerter>
   );
 };
 
